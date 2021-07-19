8a45f7a5cc3abad3ba02efd8fb06a9b9264bd098
Noye Samuel <samule.noye@amalitech.org>
2021-07-19 13:02:45 +0000

    first commit

---
+++ kelvinWeather.js
@@ -0,0 +1,11 @@
+//assign value to kelvin
+const kelvin = 292
+    //assign value to celsius
+let celsius = kelvin - 273
+    //calculate Fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32
+    //convert Fahrenheit to whole number
+fahrenheit = Math.floor(celsius * (9 / 5) + 32)
+console.log(`The temperature is ${celsius} degrees ${fahrenheit}.`)
+let newton = celsius * (33 / 100)
+console.log(Math.floor(newton))
